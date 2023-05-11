f4e827a735982279d902b2697897bbfaccfeae5d
eganj <egan.johnson@utdallas.edu>
2023-05-10 21:39:25 -0500

    remove old and unnecessary files

--- data/build_catalog.py
+++ data/build_catalog.py
@@ -9,20 +9,18 @@ import random
 from os import path
 
 steel_dir = path.join(path.dirname(__file__), "steel_defect")
-aitex_dir = path.join(path.dirname(__file__), "aitex")
 
-for dir in [steel_dir, aitex_dir]:
-    images = path.join(dir, "processed", "images")
+images = path.join(steel_dir, "processed", "images")
 
-    names = os.listdir(images)
-    random.shuffle(names)
+names = os.listdir(images)
+random.shuffle(names)
 
-    split_ind = int(len(names) * prop_test)
-    test_set = names[:split_ind]
-    train_set = names[split_ind:]
+split_ind = int(len(names) * prop_test)
+test_set = names[:split_ind]
+train_set = names[split_ind:]
 
-    with open(path.join(dir, "test_catalog.txt"), "w") as test_catalog:
-        test_catalog.write("\n".join(test_set))
+with open(path.join(steel_dir, "test_catalog.txt"), "w") as test_catalog:
+    test_catalog.write("\n".join(test_set))
 
-    with open(path.join(dir, "train_catalog.txt"), "w") as train_catalog:
-        train_catalog.writelines("\n".join(train_set))
+with open(path.join(steel_dir, "train_catalog.txt"), "w") as train_catalog:
+    train_catalog.writelines("\n".join(train_set))
--- train.sh
+++
@@ -1,3 +0,0 @@
-python3 -m training_script --data steel --model simplefcn --lr 1e-3 --device cuda:0
-python3 -m training_script --data steel --model unet_v2 --lr 1e-3 --device cuda:0
-python3 -m training_script --data steel --model unet_paper --lr 1e-3 --device cuda:0
